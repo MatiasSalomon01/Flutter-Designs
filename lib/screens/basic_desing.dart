46a288c00022da633304b5ec72e4634d2d05a46a
=Matias Salomon <=>
2023-02-22 16:45:01 -0300

    Diseño Basico - Parte 2

--- lib/screens/basic_desing.dart
+++ lib/screens/basic_desing.dart
@@ -5,9 +5,48 @@ class BasicDesignScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Image(image: AssetImage('assets/landscape.jpg')),
+    return Scaffold(
+      body: Column(
+        children: [
+          Image(image: AssetImage('assets/landscape.jpg')),
+          Title(),
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                'Oeschinen Lake Campground',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Kanderteg, Switzerland',
+                style: TextStyle(color: Colors.black),
+              )
+            ],
+          ),
+          Expanded(child: Container()),
+          const Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          const Text('41')
+        ],
       ),
     );
   }
